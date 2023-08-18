56be557740812caaa704cbbf8abe8c060c90a0c7
SMatute <santos.matute@serfinlatam.com>
2023-08-18 11:24:25 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Tarjeta{
+titulo:string;
+subtitulo:string;
+nro?: number;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Primer_Proyecto';
+  public arregloTarjetas: Tarjeta[]=[];
+
+  ngOnInit(): void {
+
+    this.arregloTarjetas =[
+      {titulo:"video 1", subtitulo:"subtitulo video 1"},
+      {titulo:"video 2", subtitulo:"subtitulo video 2"},
+      {titulo:"video 3", subtitulo:"subtitulo video 3"}
+    ]
+  }
 }
